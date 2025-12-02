f56ef9acb4aef5d26302164c95ba54903fd7a1db
Precisao-collab <rafael.sztajn@precisaoadm.com.br>
2025-12-02 15:45:21 -0300

    Add files via upload

--- src/main/java/com/precisao/recibo/controller/EmpreendimentoController.java
+++ src/main/java/com/precisao/recibo/controller/EmpreendimentoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/empreendimentos")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class EmpreendimentoController {
 
     @Value("${api.empreendimento.url}")
--- src/main/java/com/precisao/recibo/controller/ReciboController.java
+++ src/main/java/com/precisao/recibo/controller/ReciboController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/recibos")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ReciboController {
 
     private final PdfGeracaoService pdfGeracaoService;
